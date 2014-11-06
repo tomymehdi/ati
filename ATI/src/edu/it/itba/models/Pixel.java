ea5f8dc411ce0a5caf453084cd9610e423ced701
Tomas <tomymehdi@gmail.com>
2014-11-06 13:33:31 -0300

    equals, hashcode Pixel.class

--- ATI/src/edu/it/itba/models/Pixel.java
+++ ATI/src/edu/it/itba/models/Pixel.java
@@ -9,4 +9,33 @@ public class Pixel {
 		this.col = col;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + col;
+		result = prime * result + row;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pixel other = (Pixel) obj;
+		if (col != other.col)
+			return false;
+		if (row != other.row)
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+
 }
