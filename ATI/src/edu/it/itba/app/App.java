8c985f39adf0358a1e514c9d29361e6dae049194
Tomas Mehdi <tomymehdi@gmail.com>
2016-01-20 01:09:19 -0300

    Tesseract working

--- ATI/src/edu/it/itba/app/App.java
+++ ATI/src/edu/it/itba/app/App.java
@@ -2,16 +2,34 @@ package edu.it.itba.app;
 
 import edu.it.itba.swing.frames.ATIJFrameImpl;
 
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+import java.io.File;
+
 public class App {
 
+//	public static void main(String[] args) {
+//		// Schedule a job for the event-dispatching thread:
+//		// creating and showing this application's GUI.
+//		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+//			@Override
+//			public void run() {
+//				new ATIJFrameImpl();
+//			}
+//		});
+//	}
+	
 	public static void main(String[] args) {
-		// Schedule a job for the event-dispatching thread:
-		// creating and showing this application's GUI.
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				new ATIJFrameImpl();
-			}
-		});
-	}
+        File imageFile = new File("/Users/tomymehdi/itba/ati/ATI/resources/tesseract4jtests/tesseract4j-test2.jpg");
+        Tesseract instance = Tesseract.getInstance();  // JNA Interface Mapping
+//         Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
+
+        try {
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
